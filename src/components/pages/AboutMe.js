ea982ef6e2601b6c6ba55b8d0d8b187e40209b89
Dahlia <rlally89@gmail.com>
2022-12-06 07:20:43 -0800

    about me page responsive

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -4,11 +4,11 @@ import '../../styles/AboutMe.css';
 
 function AboutMe() {
     return (
-        <div className='aboutContainer justify-content-center'>
+        <div className='aboutContainer'>
             <h1 className="aboutText ">About Me</h1>
             <div className="about ">
                 <div>
-                    <Image className="aboutImg fluid" src={require(`../../assets/Rachel Lally.png`)} alt={`Rachel with her dog`} />
+                    <Image style={{width: '80%'}} className="aboutImg fluid" src={require(`../../assets/Rachel Lally.png`)} alt={`Rachel with her dog`} />
                 </div>
                 <div>
                     <p className="aboutText">My name is Rachel Lally, a soon-to-be graduate of the University of Washington Full-Stack Web Development Certificate Program.   I am based out of White Salmon, Washington. I have gained experience as a front-end and back-end developer.  As a junior developer, I am motivated to continue learn and grow in this industry.</p>
