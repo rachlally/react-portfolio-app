121528a7c38dd23c8a6c7c7dfd4d7071f97c86e0
Dahlia <rlally89@gmail.com>
2022-12-30 22:10:12 -0800

    about me updates

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -4,22 +4,18 @@ import '../../styles/AboutMe.css';
 
 function AboutMe() {
     return (
-        <div className="aboutBox">
-        <div className='aboutContainer'>
-            <div>
-                <h1 className="aboutText ">About Me</h1>
-                <div className="about ">
-                   
-                        <Image style={{ width: '20%' }} className="aboutImg fluid" src={require(`../../assets/Headshot.png`)} alt={`Rachel's Headshot`} />
-                    
-                    <div>
-                        <h4 className="aboutText">My name is Rachel Lally.  I am a Full-Stack Web Developer based out of White Salmon, Washington.  I am passionate about helping clients bring their vision to fruition.  I look forward to connecting!</h4>
-                        
-                    </div>
-                </div>
+        <div>
+            <h1 className="aboutText">About Me</h1>
+            <div className="about ">
+
+                <Image style={{ width: '20%' }} className="aboutImg fluid" src={require(`../../assets/Headshot.png`)} alt={`Rachel's Headshot`} />
+
+                <div className="blurb">
+                    <h4 className="aboutText">My name is Rachel Lally, a Full-Stack Web Developer based out of White Salmon, Washington.  I am passionate about helping clients bring their vision to life. </h4>
+
                 </div>
             </div>
-            </div>
+        </div>
     );
 }
 
